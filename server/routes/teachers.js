02526dfbc8a229bd767da3a6cc48c742788d306f
Rachel Cavin <Rmcavin@gmail.com>
2018-01-09 13:56:27 -0600

    add bcrypt functionality again

--- server/routes/teachers.js
+++ server/routes/teachers.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const server = express();
 const knex = require('../db/knex');
-//const bcrypt = require('bcrypt');
-//const saltRounds = 12;
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
 
 //get all the teachers
 server.get('/teachers', (req, res) => {
@@ -15,25 +15,25 @@ server.get('/teachers/:id', (req, res) => {
 })
 
 //register new teachers
-// server.post('/teachers/register', (req, res) => {
-//   (req, res) => {
-//     let username = req.body.teacherUserName;
-//     let firstName = req.body.firstName;
-//     let lastName = req.body.lastName;
-//     let title = req.body.title;
-//     bcrypt.hash(req.body.teacherPassWord, saltRounds, (err, hash) => {
-//       let user = new User({username:username, password:hash, firstName:firstName, lastName:lastName, title:title});
-//       knex('teachers')
-//         insert(user)
-//         .then((teachers) => {
-//           res.json(teachers[0])
-//         })
-//         .catch( (err) => {
-//           next(err);
-//         });
-//     })
-//   }
-// })
+server.post('/teachers/register', (req, res) => {
+  (req, res) => {
+    let username = req.body.teacherUserName;
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    let title = req.body.title;
+    bcrypt.hash(req.body.teacherPassWord, saltRounds, (err, hash) => {
+      let user = new User({username:username, password:hash, firstName:firstName, lastName:lastName, title:title});
+      knex('teachers')
+        insert(user)
+        .then((teachers) => {
+          res.json(teachers[0])
+        })
+        .catch( (err) => {
+          next(err);
+        });
+    })
+  }
+})
 
 //log in a teachers
 server.post('/teachers/login', (req, res) => {
